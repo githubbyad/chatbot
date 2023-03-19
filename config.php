00340454e5dab056c8124c87f344e9f6cf1660e7
Adnan <73897123+githubbyad@users.noreply.github.com>
2023-03-19 14:57:48 +0530

    Update config.php

--- config.php
+++ config.php
@@ -12,7 +12,8 @@ $dsn = "sqlsrv:Server=$host;Database=$db";
 // set attributes
 $options = [
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
-    PDO::ATTR_EMULATE_PREPARES => false
+    PDO::ATTR_EMULATE_PREPARES => false,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ];
 try {
     // create PDO instance
