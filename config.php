6d94aaf8bf61b6bb5aba2d48ac5b6a86b3695379
Adnan <73897123+githubbyad@users.noreply.github.com>
2023-03-19 12:18:23 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = "localhost";
-$user = "id20183698_root";
-$pwd = "VCc{MW#d%+X)%Ml6";
-$db = "id20183698_response";
+$user = "root";
+$pwd = "password";
+$db = "chatbot";
 
 // set DSN
 $dsn = "mysql:host=$host;dbname=$db";
